694a894a29568fbb000db6be1b229942153037e0
Diego Lorenzo <diego@diegolorenzo.com>
2016-11-13 21:53:45 -0500

    Ugh... another fix to routes.

--- src/routes/web.php
+++ src/routes/web.php
@@ -1,13 +1,13 @@
 <?php
 
 // Admin Routes
-Route::group(['namespace' => 'Admin', 'prefix' => 'admin'], function() {
-    Route::get('{page}', 'Weerd\ApolloPages\Http\Controllers\Admin\PageController@show');
+Route::group(['namespace' => 'Weerd\ApolloPages\Http\Controllers\Admin', 'prefix' => 'admin'], function() {
+    Route::get('{page}', 'PageController@show');
 });
 
 
 // Client Routes
-Route::group(['namespace' => 'Client'], function () {
-    Route::get('{page}/{category}', 'Weerd\ApolloPages\Http\Controllers\Client\PageController@show');
-    Route::get('{page}', 'Weerd\ApolloPages\Http\Controllers\Client\PageController@show');
+Route::group(['namespace' => 'Weerd\ApolloPages\Http\Controllers\Client'], function () {
+    Route::get('{page}/{category}', 'PageController@show');
+    Route::get('{page}', 'PageController@show');
 });
