669de8c27f1e03c19372574acd047ffb273e730c
Diego Lorenzo <diego@diegolorenzo.com>
2016-12-28 14:16:54 -0500

    Adding validation, showing errors, and assigning processed attributes for page creation.

--- src/Models/ApolloPage.php
+++ src/Models/ApolloPage.php
@@ -20,13 +20,103 @@ class ApolloPage extends Model
      */
     protected $guarded = ['id', 'created_at', 'updated_at'];
 
-	/**
-	 * Find the specified page by its slug.
-	 *
-	 * @param string $slug
-	 */
+    /**
+     * Find the specified page by its slug.
+     *
+     * @param string $slug
+     */
     public static function findBySlug($slug)
     {
         return self::where('slug', $slug)->first();
+    }
+
+    /**
+     * Assign page attributes that need to be processed first.
+     *
+     * @param  \Illuminate\Http\Request $request
+     * @return \Illuminate\Http\Request
+     */
+    public static function assignProcessedAttributes($request)
+    {
+        $request['slug'] = static::assignSlugAttribute($request);
+
+        $request['path'] = static::assignPathAttribute($request);
+
+        $request['tier'] = static::assignTierAttribute($request);
+
+        return $request;
+    }
+
+    /**
+     * Assign path attribute based on slug and if page contains a parent id.
+     *
+     * @param  \Illuminate\Http\Request $request
+     * @return string
+     */
+    public static function assignPathAttribute($request)
+    {
+        $parent = static::where('parent_id', $request->input('parent_id'))->first();
+
+        if ($parent) {
+            return $parent->path.'/'.$request->input('slug');
+        }
+
+        return $request->input('slug');
+    }
+
+    /**
+     * Assign slug attirbute based on provided value or the page title.
+     *
+     * @param  \Illuminate\Http\Request $request
+     * @return string
+     */
+    public static function assignSlugAttribute($request)
+    {
+        $value = $request->input('slug');
+
+        if (empty($value)) {
+            $value = $request->input('title');
+        }
+
+        return str_slug($value);
+    }
+
+    /**
+     * Assign tier attribute based on whether page is nested under a parent page.
+     *
+     * @param  \Illuminate\Http\Request $request
+     * @return int
+     */
+    public static function assignTierAttribute($request)
+    {
+        $parent = static::where('id', $request->input('parent_id'))->first();
+
+        if ($parent) {
+            return $parent->tier + 1;
+        }
+
+        return 1;
+    }
+
+    /**
+     * Set the slug for the page.
+     *
+     * @param  string $value
+     * @return void
+     */
+    public function setSlugAttribute($value)
+    {
+        $this->attributes['slug'] = str_slug($value);
+    }
+
+    /**
+     * Set the parent id for the page.
+     *
+     * @param  string $value
+     * @return void
+     */
+    public function setParentIdAttribute($value)
+    {
+        $this->attributes['parent_id'] = empty($value) ? null : $value;
     }
 }
