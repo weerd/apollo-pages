ed7d98f667db285cd75caef6abcffec5a6bb0562
Diego Lorenzo <diego@diegolorenzo.com>
2017-07-09 03:42:25 -0400

    Adding routes that can be overridden in project.

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,19 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('apollo', function() {
-    return 'apollo pages, woot woot!';
+    return 'Apollo Pages routes are working.';
+});
+
+/*
+| The following Page routes need be attempted last.
+*/
+
+// Admin Routes
+Route::group(['namespace' => 'Weerd\ApolloPages\Http\Controllers\Admin', 'prefix' => 'admin'], function() {
+    Route::resource('pages', 'PageController', ['as' => 'admin']);
+});
+
+// Client Routes
+Route::group(['namespace' => 'Weerd\ApolloPages\Http\Controllers\Client'], function () {
+    Route::get('{params?}', 'PageController@show')->where('params', '(.*)')->name('client.pages.show');
 });
